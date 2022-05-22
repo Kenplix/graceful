ffe3edd2478c20869bd80f3d134731efe64c2bd1
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2022-05-22 15:44:20 +0300

    working draft with example

---
+++ container.go
@@ -0,0 +1,83 @@
+package graceful
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+)
+
+type Container struct {
+	breakers []breaker
+	mu       sync.Mutex
+	once     sync.Once
+}
+
+type Func func(ctx context.Context) error
+
+type breaker struct {
+	name string
+	fn   Func
+}
+
+func (c *Container) Add(name string, fn Func) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.breakers = append(c.breakers, breaker{
+		name: name,
+		fn:   fn,
+	})
+}
+
+func (c *Container) Shutdown(ctx context.Context) (err error) {
+	c.once.Do(func() { err = c.shutdown(ctx) })
+	return err
+}
+
+type ShutdownError struct {
+	Messages []string
+}
+
+func (e ShutdownError) Error() string {
+	return fmt.Sprintf("shutdown finished with error(s): \n%s", strings.Join(e.Messages, "\n"))
+}
+
+func (c *Container) shutdown(ctx context.Context) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var (
+		messages = make([]string, 0, len(c.breakers))
+		complete = make(chan struct{}, 1)
+	)
+
+	go func() {
+		for i := len(c.breakers) - 1; i >= 0; i-- {
+			log.Printf("shutting down %q gracefully", c.breakers[i].name)
+
+			if err := c.breakers[i].fn(ctx); err != nil {
+				messages = append(messages, fmt.Sprintf("[!] %v", err))
+			}
+
+			log.Printf("shutting down %q complete", c.breakers[i].name)
+		}
+
+		c.breakers = c.breakers[0:0]
+		complete <- struct{}{}
+	}()
+
+	select {
+	case <-complete:
+		break
+	case <-ctx.Done():
+		return fmt.Errorf("shutdown cancelled: %v", ctx.Err())
+	}
+
+	if len(messages) > 0 {
+		return ShutdownError{Messages: messages}
+	}
+
+	return nil
+}
---
+++ examples/example.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"graceful"
+	"log"
+	"net"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Service struct {
+	*http.Server
+}
+
+func NewService(ctx context.Context, addr string) *Service {
+	return &Service{
+		Server: &http.Server{
+			Addr: addr,
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				ctx := r.Context()
+
+				for {
+					select {
+					case <-ctx.Done():
+						log.Printf("shutting down connection with %s gracefully", r.RemoteAddr)
+						w.WriteHeader(http.StatusOK)
+						return
+					case <-time.After(time.Second):
+						log.Printf("connection with %s alive", r.RemoteAddr)
+					}
+				}
+			}),
+			BaseContext: func(_ net.Listener) context.Context {
+				return ctx
+			},
+		},
+	}
+}
+
+func (s *Service) Run(ctx context.Context) error {
+	failure := make(chan error)
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			failure <- fmt.Errorf("listen and serve: %w", err)
+		}
+	}()
+
+	log.Printf("listening on %s", s.Addr)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case err := <-failure:
+			return err
+		case <-time.After(time.Second):
+			log.Printf("service at %s alive", s.Addr)
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	svc := NewService(ctx, "127.0.0.1:8080")
+
+	var ctr graceful.Container
+	ctr.Add("service", svc.Shutdown)
+
+	defer func(ctx context.Context) {
+		if err := ctr.Shutdown(ctx); err != nil {
+			log.Println(err)
+		}
+	}(context.TODO())
+
+	if err := svc.Run(ctx); err != nil {
+		log.Printf("exit reason: %s", err)
+	}
+}
